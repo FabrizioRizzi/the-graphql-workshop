99e9cbb2a87a88497a6eca8581ea2c7f4945c996
Fabrizio Rizzi <fabrizio.rizzi@nearform.com>
2025-01-03 13:05:41 +0100

    Ex 6

--- src/my-solution/server.js
+++ src/my-solution/server.js
@@ -1,8 +1,29 @@
 import buildServer from './index.js'
 
-const fastify = buildServer()
-
 const startServer = async () => {
+  const fastify = buildServer()
+  await fastify.ready()
+
+  fastify.graphql.addHook('preParsing', async function () {
+    fastify.log.info('preParsing called')
+  })
+
+  fastify.graphql.addHook('preValidation', async function () {
+    fastify.log.info('preValidation called')
+  })
+
+  fastify.graphql.addHook('preExecution', async function (schema, document) {
+    fastify.log.info('preExecution called')
+    /* return {
+      document,
+      errors: [new Error('foo')]
+    } */
+  })
+
+  fastify.graphql.addHook('onResolution', async function () {
+    fastify.log.info('onResolution called')
+  })
+
   try {
     await fastify.listen({ port: 3000 })
   } catch (err) {
